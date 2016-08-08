90db048c3825a40c5281028498a4b3e151977567
Martin Řezník <martin@yosarin.net>
2016-08-08 02:54:08 +0200

    adding mutexes to avoid issues with concurent access

--- lib/list.go
+++ lib/list.go
@@ -2,6 +2,7 @@ package lib
 
 import (
 	"errors"
+	"sync"
 )
 
 // ListItem - Item in List of files
@@ -17,19 +18,23 @@ type hasFileName interface {
 // UniqueList - List of items
 type UniqueList struct {
 	list map[string]ListItem
+	m    *sync.Mutex
 }
 
 // NewUniqueList - will create new file list
 func NewUniqueList() *UniqueList {
 	l := &UniqueList{
 		make(map[string]ListItem),
+		&sync.Mutex{},
 	}
 	return l
 }
 
 // Add - will add item into file list
 func (l *UniqueList) Add(i ListItem) {
+	l.m.Lock()
 	l.list[i.ID()] = i
+	l.m.Unlock()
 }
 
 // AddUniq - will add unique value
@@ -43,7 +48,9 @@ func (l *UniqueList) AddUniq(item ListItem, log LogI) (err error) {
 
 // Find - finds and returns item by id
 func (l *UniqueList) Find(id string) (ListItem, error) {
+	l.m.Lock()
 	s, ok := l.list[id]
+	l.m.Unlock()
 	if !ok {
 		return nil, errors.New("Item not found")
 	}
@@ -52,14 +59,17 @@ func (l *UniqueList) Find(id string) (ListItem, error) {
 
 // Delete - will delete item from list
 func (l *UniqueList) Delete(id string) {
+	l.m.Lock()
 	l.list[id].OnRemove()
 	delete(l.list, id)
+	l.m.Unlock()
 }
 
 // FileNames - will return list of all songs file names
 func (l *UniqueList) FileNames() []string {
 	out := make([]string, len(l.list))
 	i := 0
+	l.m.Lock()
 	for _, val := range l.list {
 		v, ok := val.(hasFileName)
 		if ok {
@@ -67,19 +77,23 @@ func (l *UniqueList) FileNames() []string {
 			i++
 		}
 	}
+	l.m.Unlock()
 	return out
 }
 
 // FindByFile - finds if we already have this file prepared
 func (l *UniqueList) FindByFile(filename string) ListItem {
+	l.m.Lock()
 	for _, s := range l.list {
 		v, ok := s.(hasFileName)
 		if ok {
 			if v.FileName() == filename {
+				l.m.Unlock()
 				return s
 			}
 		}
 	}
+	l.m.Unlock()
 	return nil
 }
 
